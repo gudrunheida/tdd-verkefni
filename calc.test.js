ea1eceb45979ce6508e5a65c458aa587904c5e0a
Gudrun Heida <gudrunb16@ru.is>
2018-10-11 23:23:00 +0000

    implemented for sum

--- calc.test.js
+++ calc.test.js
@@ -9,3 +9,7 @@ it('Should return zero on an empty string', () => {
 it('Should return when only one number is in the string', () => {
     expect(add("5")).toBe(5);
 });
+
+it('Should return sum of two numbers', () => {
+    expect(add("2,4")).toBe(6);
+});
