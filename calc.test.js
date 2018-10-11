53588a53e2f748099b256dce4a8abf8413abc195
Gudrun Heida <gudrunb16@ru.is>
2018-10-11 23:14:40 +0000

    implemented for one number

--- calc.test.js
+++ calc.test.js
@@ -3,3 +3,9 @@ const add = require('./calc');
 it('Should return zero on an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+
+
+it('Should return when only one number is in the string', () => {
+    expect(add("5")).toBe(5);
+});
