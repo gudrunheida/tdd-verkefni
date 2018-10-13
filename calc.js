28a2cb2a25b7dad4a8a5c3a95f941a31a26ff12e
Gudrun Heida <gudrunb16@ru.is>
2018-10-13 18:15:48 +0000

    added test for negative numbers

--- calc.js
+++ calc.js
@@ -5,12 +5,36 @@ function add(number) {
     if (number.includes(",")) {
         var numberArray = number.split(",");
         var total = 0;
+        var negativeNumbers = []
         for (var i = 0; i < numberArray.length; i++) {
+            if (numberArray[i] < 0) {
+                negativeNumbers.push(numberArray[i])
+            }
             total += parseInt(numberArray[i]);
         }
+        if (negativeNumbers.length !== 0) {
+            var returnStr = "Negative numbers not allowed:";
+            for (var j = 0; j < negativeNumbers.length; j++) {
+                returnStr += " " + negativeNumbers[j]
+            }
+            return returnStr
+        }
+
         return total;
     } else
         return parseInt(number);
 }
 
 module.exports = add;
+
+
+// function negativeNumbers = new Array();
+// if (number < 0) {
+//     negativeNumbers.add(num);
+// }
+
+// var error = "negative numbers not allowed"
+// if (negativeNumbers.size() > 0) {
+//     return error;
+
+// }
--- calc.test.js
+++ calc.test.js
@@ -15,3 +15,7 @@ it('Should return sum of two numbers', () => {
 it('Should return sumerize multiple numbers', () => {
     expect(add("2,4,3,1")).toBe(10);
 });
+
+it("should be able to throw an error and print out a negative number", () => {
+    expect(add('2,-8,-9')).toBe("Negative numbers not allowed: -8 -9");
+});
